314c61760d9980957e2b1e1cc65b8e3a819dba73
周兴婷 <1404203081@qq.com>
2019-07-29 09:54:47 +0800

    svg文件嵌套

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -104,6 +104,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- <style lang="less" scoped> -->
 <style lang="less" scoped>
 h3 {
   color: @color;
--- src/icons.js
+++ src/icons.js
@@ -4,3 +4,6 @@ const requireAll = requireContext => requireContext.keys().map(requireContext);
 // import all svg
 const req = require.context('./assets/icons', false, /\.svg$/);
 requireAll(req);
+
+const reqSvg = require.context('./assets/icons/svg', false, /\.svg$/);
+requireAll(reqSvg);
