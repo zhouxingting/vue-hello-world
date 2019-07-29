4e61a05411254b17f3517692c3b4e4c39f24b6db
周兴婷 <1404203081@qq.com>
2019-07-29 10:01:54 +0800

    处理icons子文件夹

--- src/icons.js
+++ src/icons.js
@@ -2,8 +2,5 @@
 const requireAll = requireContext => requireContext.keys().map(requireContext);
 
 // import all svg
-const req = require.context('./assets/icons', false, /\.svg$/);
+const req = require.context('./assets/icons', true, /\.svg$/);
 requireAll(req);
-
-const reqSvg = require.context('./assets/icons/svg', false, /\.svg$/);
-requireAll(reqSvg);
