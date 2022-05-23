37192f87beac02965f12c422fc34195241534f15
zhouxingting@newrank.cn <1404203081@qq.com>
2022-05-23 17:01:37 +0800

    base

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,25 @@
-const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
+const LodashModuleReplacementPlugin = require("lodash-webpack-plugin");
 
-const path = require('path');
-const port = process.env.port || process.env.npm_config_port || 9000 // dev port
-const name = 'vue Demo'
+const path = require("path");
+const port = process.env.port || process.env.npm_config_port || 9000; // dev port
+const name = "vue Demo";
 
 function resolve(dir) {
-  return path.join(__dirname, dir)
+  return path.join(__dirname, dir);
 }
 
 function addStyleResource(rule) {
   rule
-    .use('style-loader')
-    .loader('style-resources-loader')
+    .use("style-loader")
+    .loader("style-resources-loader")
     .options({
-      patterns: [path.resolve(__dirname, 'src/assets/common/variable.less')],
+      patterns: [path.resolve(__dirname, "src/assets/common/variable.less")]
     });
 }
 
 module.exports = {
-  lintOnSave: process.env.NODE_ENV !== 'production',
+  lintOnSave: process.env.NODE_ENV !== "production",
+  baseUrl: "/vue-hello-world",
   devServer: {
     port: port,
     open: true,
@@ -27,13 +28,13 @@ module.exports = {
       errors: true
     },
     proxy: {
-      '/api': {
-        target: 'http://dev.91zdd.cn',
+      "/api": {
+        target: "http://dev.91zdd.cn",
         changeOrigin: true,
         pathRewrite: {
-          '^/api': '',
-        },
-      },
+          "^/api": ""
+        }
+      }
     }
   },
   productionSourceMap: false,
@@ -43,99 +44,94 @@ module.exports = {
     name: name,
     resolve: {
       alias: {
-        '@': resolve('src')
+        "@": resolve("src")
       }
     }
   },
   configureWebpack: {
     plugins: [
       new LodashModuleReplacementPlugin({
-        paths: true,
-      }),
+        paths: true
+      })
     ]
   },
-  chainWebpack: (config) => {
-    const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
-    types.forEach(type => addStyleResource(config.module.rule('less').oneOf(type))); //自动全局引入公共less文件
+  chainWebpack: config => {
+    const types = ["vue-modules", "vue", "normal-modules", "normal"];
+    types.forEach(type => addStyleResource(config.module.rule("less").oneOf(type))); //自动全局引入公共less文件
 
     // set svg-sprite-loader webpack-chain
     config.module
-      .rule('svg')
-      .exclude.add(resolve('src/assets/icons'))
-      .end()
+      .rule("svg")
+      .exclude.add(resolve("src/assets/icons"))
+      .end();
     config.module
-      .rule('icons')
+      .rule("icons")
       .test(/\.svg$/)
-      .include.add(resolve('src/assets/icons'))
+      .include.add(resolve("src/assets/icons"))
       .end()
-      .use('svg-sprite-loader')
-      .loader('svg-sprite-loader')
+      .use("svg-sprite-loader")
+      .loader("svg-sprite-loader")
       .options({
-        symbolId: '[name]'
+        symbolId: "[name]"
       })
-      .end()
+      .end();
 
-      // set preserveWhitespace
-      config.module
-        .rule('vue')
-        .use('vue-loader')
-        .loader('vue-loader')
-        .tap(options => {
-          options.compilerOptions.preserveWhitespace = true
-          return options
-        })
-        .end()
-      config
+    // set preserveWhitespace
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
+      .tap(options => {
+        options.compilerOptions.preserveWhitespace = true;
+        return options;
+      })
+      .end();
+    config
       // https://webpack.js.org/configuration/devtool/#development
-      .when(process.env.NODE_ENV === 'development',
-        config => config.devtool('cheap-source-map')
-      )
+      .when(process.env.NODE_ENV === "development", config => config.devtool("cheap-source-map"));
+    config.when(process.env.NODE_ENV !== "development", config => {
       config
-      .when(process.env.NODE_ENV !== 'development',
-        config => {
-          config
-            .plugin('ScriptExtHtmlWebpackPlugin')
-            .after('html')
-            .use('script-ext-html-webpack-plugin', [{
+        .plugin("ScriptExtHtmlWebpackPlugin")
+        .after("html")
+        .use("script-ext-html-webpack-plugin", [
+          {
             // `runtime` must same as runtimeChunk name. default is `runtime`
-              inline: /runtime\..*\.js$/
-            }])
-            .end()
-          config.plugin('AutoDllPlugin')
-          .use('autodll-webpack-plugin-4x', [{
+            inline: /runtime\..*\.js$/
+          }
+        ])
+        .end();
+      config
+        .plugin("AutoDllPlugin")
+        .use("autodll-webpack-plugin-4x", [
+          {
             inject: true, // will inject the DLL bundle to index.html
             debug: true,
-            filename: '[name]_bundle.js',
-            path: './dll',
+            filename: "[name]_bundle.js",
+            path: "./dll",
             entry: {
-              webpack: [
-                'vue',
-                'vue-router',
-                'vuex'
-              ],
-            },
-          }])
-          .end()
-          config
-            .optimization.splitChunks({
-              chunks: 'all',
-              cacheGroups: {
-                elementUI: {
-                  name: 'chunk-elementUI', // split elementUI into a single package
-                  priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-                  test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
-                },
-                commons: {
-                  name: 'chunk-commons',
-                  test: resolve('src/components'), // can customize your rules
-                  minChunks: 3, //  minimum common number
-                  priority: 5,
-                  reuseExistingChunk: true
-                }
-              }
-            })
-          config.optimization.runtimeChunk('single')
+              webpack: ["vue", "vue-router", "vuex"]
+            }
+          }
+        ])
+        .end();
+      config.optimization.splitChunks({
+        chunks: "all",
+        cacheGroups: {
+          elementUI: {
+            name: "chunk-elementUI", // split elementUI into a single package
+            priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+            test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+          },
+          commons: {
+            name: "chunk-commons",
+            test: resolve("src/components"), // can customize your rules
+            minChunks: 3, //  minimum common number
+            priority: 5,
+            reuseExistingChunk: true
+          }
         }
-      )
-  },
+      });
+      config.optimization.runtimeChunk("single");
+    });
+  }
 };
